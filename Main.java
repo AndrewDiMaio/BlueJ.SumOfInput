7d88f2835fdf860864eacf3f8b176f39a008a3b4
Andrew DiMaio <andrewdimaio@zipcoders-MacBook-Pro.local>
2019-05-22 13:35:08 -0400

    created Sum Of Input calculator

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-
-    }
+        Scanner number = new Scanner(System.in);
+        System.out.println("Pick a number: ");
+        int pickedNumber = number.nextInt();
+        System.out.println("User Picked: " + pickedNumber);
+        
+        int total = 0;
+        for (Integer i = 0; i < pickedNumber; i++){
+            total+=i+1;
+        }
+        System.out.println(total);
+}
 }
